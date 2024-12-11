8914f3251d14ac26b6e300b0f9448095b6f0cee4
habacuque-gosch <habacuke14@gmail.com>
2024-12-10 21:49:50 -0300

    create model Item, and make migrations and migrate migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Item(models.Model):
+    name = models.CharField(default='', max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        self.name
