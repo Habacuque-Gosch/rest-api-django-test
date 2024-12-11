b496248503cf4584817f69f7505f1c4664658ad8
habacuque-gosch <habacuke14@gmail.com>
2024-12-11 18:59:08 -0300

    alter models Item, null and blank false for all fields

--- rest_api/models.py
+++ rest_api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(default='', max_length=200, null=True, blank=True)
-    password = models.CharField(default='', max_length=50, null=True, blank=True)
+    name = models.CharField(default='', max_length=200, null=False, blank=False)
+    password = models.CharField(default='', max_length=50, null=False, blank=False)
 
     def __str__(self):
         return self.name
