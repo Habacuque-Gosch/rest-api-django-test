7bec63def82b7e7ecde1389adf18e980e6ab1edb
habacuque-gosch <habacuke14@gmail.com>
2024-12-11 18:15:22 -0300

    create endpoint manage user

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,7 +3,6 @@ from .views.views import *
 
 urlpatterns = [
     path('', get_users),
-    path('add/', add_user),
-    path('users/<str: name>', get_by_name),
-
+    path('users/<str:name>', get_by_name),
+    path('manager', user_manager),
 ]
--- rest_api/views/views.py
+++ rest_api/views/views.py
@@ -24,24 +24,55 @@ def get_users(request):
 
     return Response(status=status.HTTP_404)
 
+@api_view(['GET'])
+def get_by_name(request, name):
 
-@api_view(['POST'])
-def add_user(request):
+    try:
+        user = Item.objects.get(name=name)
 
-    if request.method == 'POST':
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+
+        serializer = ItemSerializer(user)
+        return Response(serializer.data)
+    
+@api_view(['GET','POST', 'PUT', 'DELETE'])
+def user_manager(request):
+    
+    if request.method == 'GET':
 
         try:
-            serializer = ItemSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-            
-            return Response(serializer.data)
-        
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)  
-    
-    return Response(status=status.HTTP_404_NOT_FOUND)
+            if request.GET['name']:
 
-@api_view(['GET'])
-def get_by_name(request):
-    user = Item.objects.get(name=request.name)
+                name = request.GET['name']
+
+                try:
+                    user = Item.objects.get(name=name)
+                except:
+                    print('user not found')
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+                
+                serializer = ItemSerializer(user)
+                return Response(serializer.data)
+            
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    # if request.method == 'POST':
+
+    #     try:
+    #         serializer = ItemSerializer(data=request.data)
+    #         if serializer.is_valid():
+    #             serializer.save()
+            
+    #         return Response(serializer.data)
+        
+    #     except:
+    #         return Response(status=status.HTTP_404_NOT_FOUND)  
+    
+    # return Response(status=status.HTTP_404_NOT_FOUND)
