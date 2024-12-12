123851dd7b7ef066d15e1cb2afa452e5cede4cde
habacuque-gosch <habacuke14@gmail.com>
2024-12-12 11:18:32 -0300

    redefined cors_allowed and disable debug for production

--- setup/settings.py
+++ setup/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                 'rest-api-users-y18n.onrender.com',
