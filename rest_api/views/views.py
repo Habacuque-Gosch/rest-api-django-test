06645f97e8f05cf7f728fad7b2bcbe6a6f8ea4b6
habacuque-gosch <habacuke14@gmail.com>
2024-12-12 16:41:52 -0300

    comment import json and import contrin auth login policies

--- rest_api/views/views.py
+++ rest_api/views/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_api.models import Item
 from rest_api.serializers import ItemSerializer
+# from django.contrib.auth import login
 import json
 
 
