37a94857c270440209fa733dedccbcec97ab6a46
habacuque-gosch <habacuke14@gmail.com>
2024-12-13 13:48:46 -0300

    comment method

--- rest_api/models.py
+++ rest_api/models.py
@@ -7,5 +7,5 @@ class Item(models.Model):
     name = models.CharField(default='', max_length=200, null=False, blank=False)
     password = models.CharField(default='', max_length=50, null=False, blank=False)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
